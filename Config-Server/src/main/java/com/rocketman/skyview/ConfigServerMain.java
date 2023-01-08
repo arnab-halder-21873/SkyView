39f695efb959ec0eb5eeb945ee1279ddb5c32c9b
rocketman <arnabhalder164@gmail.com>
2023-01-08 17:20:46 +0530

    Config Server

--- Config-Server/src/main/java/com/rocketman/skyview/ConfigServerMain.java
+++ Config-Server/src/main/java/com/rocketman/skyview/ConfigServerMain.java
@@ -1,11 +1,16 @@
 package com.rocketman.skyview;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class,
+        SecurityAutoConfiguration.class,
+        ManagementWebSecurityAutoConfiguration.class})
 @EnableEurekaClient
 @EnableConfigServer
 public class ConfigServerMain {
